c4de58ff9b8e38761fe385c6511d1196e8b7439a
Rafael Mattos <rafael_xuvisco@yahoo.com.br>
2016-06-05 02:47:03 +0000

    Adicionado controller do paciente

--- app/Massoterapia/PacienteCadastro/Http/Controllers/PacienteCadastroController.php
+++ app/Massoterapia/PacienteCadastro/Http/Controllers/PacienteCadastroController.php
@@ -1,17 +1,58 @@
 <?php
 
-namespace App\Http\Controllers\Massoterapia\PacienteCadastro\Http\Controllers;
+namespace App\Massoterapia\PacienteCadastro\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Massoterapia\PacienteCadastro\PacienteCadastro;
+use App\Massoterapia\PacienteCadastro\Http\Validacao\PacienteCadastroValidacao;
 
 class PacienteCadastroController extends Controller
 {
     protected $pacienteCadastroNegocio;
     
-    public function __construct()
+    public function __construct(PacienteCadastro $pacienteCadastroNegocio)
     {
-        
+        $this->pacienteCadastroNegocio = $pacienteCadastroNegocio;
+    }
+    
+    public function index()
+    {
+        $pacienteCadastro = $this->pacienteCadastroNegocio->all();
+        return view('pacientecadastro.index', compact('pacienteCadastro'));
+    }
+    
+    public function create()
+    {
+        return view('pacientecadastro.index');
+    }
+    
+    public function store(PacienteCadastroValidacao $paciente)
+    {
+        $this->pacienteCadastroNegocio->save($paciente->all());
+        return redirect()->route('paciente-cadastro.index');
+    }
+    
+    public function show($id)
+    {
+    }
+    
+    public function edit($id)
+    {
+        $pacienteCadastro = $this->pacienteCadastroNegocio->find($id);
+        return view('pacientecadastro.edit', compact('pacienteCadastro'));
+    }
+    
+    public function update($id, PacienteCadastroValidacao $paciente)
+    {
+        $this->pacienteCadastroNegocio->update($id, $paciente->all());
+        return redirect()->route('paciente-cadastro.index');
+    }
+    
+    public function destroy($id)
+    {
+        $this->pacienteCadastroNegocio->delete($id);
+        return redirect()->route($id);
     }
 }
